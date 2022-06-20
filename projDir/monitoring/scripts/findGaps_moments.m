a0cb90bbc07498e0606dd239880966f03ebfbeb7
dixon <dixon@ucar.edu>
2022-06-20 12:54:58 -0600

    Monitoring for moments

---
+++ projDir/monitoring/scripts/findGaps_moments.m
@@ -0,0 +1,37 @@
+function [saveTimes,noDataTimeNan]=findGaps_moments(inType,baseDir,dateString,noDataTime,noDataTimeNan)
+
+saveFiles=dir([baseDir,inType,'/',dateString,'/*.nc']);
+saveTimes=[];
+saveDBZ=[];
+
+for jj=1:size(saveFiles,1)    
+    inName=saveFiles(jj).name;
+    baseTime=ncread([saveFiles(jj).folder,'/',inName],'time_coverage_start');
+    baseTime=baseTime';
+    baseTimeNum=datetime(str2num(baseTime(1:4)),str2num(baseTime(6:7)),str2num(baseTime(9:10)),...
+        str2num(baseTime(12:13)),str2num(baseTime(15:16)),str2num(baseTime(18:19)));
+    inTime=ncread([saveFiles(jj).folder,'/',inName],'time');
+    fileTime=baseTimeNum+seconds(inTime);
+    saveTimes=cat(1,saveTimes,fileTime);
+end
+
+saveTimeDiff=etime(datevec(saveTimes(2:end)),datevec(saveTimes(1:end-1)));
+
+if ~isempty(saveTimes)
+    goodTimes=zeros(size(saveTimeDiff));
+    goodTimes(saveTimeDiff<60)=1;
+    diffGoodTimes=diff(goodTimes);
+
+    startGoodInds=find(diffGoodTimes==1)+1;
+    endGoodInds=find(diffGoodTimes==-1)+1;
+
+    startGoodTimes=saveTimes(startGoodInds);
+    startGoodTimes=cat(1,saveTimes(1),startGoodTimes);
+    endGoodTimes=saveTimes(endGoodInds);
+    endGoodTimes=cat(1,endGoodTimes,saveTimes(end));
+
+    for kk=1:length(startGoodTimes)
+        noDataTimeNan(noDataTime>startGoodTimes(kk) & noDataTime<endGoodTimes(kk))=nan;
+    end
+end
+end
--- projDir/monitoring/scripts/find_data_gaps_timeSeries.m
+++ projDir/monitoring/scripts/find_data_gaps_timeSeries.m
@@ -3,7 +3,7 @@
 clear all
 close all
 
-dataType='spoldrx';
+dataType='sband';
 
 startDate=datetime(2022,5,23);
 endDate=datetime(2022,6,4); % This day is included
@@ -29,103 +29,13 @@ for ii=1:length(dayList)
     noDataTimeNan=-noDataTimeNan;
 
     % 'save' directory
-    saveFiles=dir([baseDir,'save/',dateString,'/*.iwrf_ts']);
-    saveTimes=[];
-    saveBytes=nan(length(saveFiles),1);
-
-    for jj=1:size(saveFiles,1)
-        inName=saveFiles(jj).name;
-        saveTimes=[saveTimes;datetime(str2num(inName(1:4)),str2num(inName(5:6)),str2num(inName(7:8)),...
-            str2num(inName(10:11)),str2num(inName(12:13)),str2num(inName(14:15)),str2num(inName(17:19)))];
-        saveBytes(jj)=saveFiles(jj).bytes;
-    end
-
-    saveTimeDiff=etime(datevec(saveTimes(2:end)),datevec(saveTimes(1:end-1)));
-    saveDataRate=saveBytes(1:end-1)./saveTimeDiff;
-
-    if ~isempty(saveTimes)
-        goodTimes=zeros(size(saveTimeDiff));
-        goodTimes(saveTimeDiff<120)=1;
-        diffGoodTimes=diff(goodTimes);
-
-        startGoodInds=find(diffGoodTimes==1)+1;
-        endGoodInds=find(diffGoodTimes==-1)+1;
-
-        startGoodTimes=saveTimes(startGoodInds);
-        startGoodTimes=cat(1,saveTimes(1),startGoodTimes);
-        endGoodTimes=saveTimes(endGoodInds);
-        endGoodTimes=cat(1,endGoodTimes,saveTimes(end));
-
-        for kk=1:length(startGoodTimes)
-            noDataTimeNan(noDataTime>startGoodTimes(kk) & noDataTime<endGoodTimes(kk))=nan;
-        end
-    end
+    [saveTimes,saveDataRate,noDataTimeNan]=findGaps_timeSeries('save',baseDir,dateString,noDataTime,noDataTimeNan);    
 
     % 'sunscan' directory
-    sunscanFiles=dir([baseDir,'sunscan/',dateString,'/*.iwrf_ts']);
-    sunscanTimes=[];
-    sunscanBytes=nan(length(sunscanFiles),1);
+    [sunscanTimes,sunscanDataRate,noDataTimeNan]=findGaps_timeSeries('sunscan',baseDir,dateString,noDataTime,noDataTimeNan);
 
-    for jj=1:size(sunscanFiles,1)
-        inName=sunscanFiles(jj).name;
-        sunscanTimes=[sunscanTimes;datetime(str2num(inName(1:4)),str2num(inName(5:6)),str2num(inName(7:8)),...
-            str2num(inName(10:11)),str2num(inName(12:13)),str2num(inName(14:15)),str2num(inName(17:19)))];
-        sunscanBytes(jj)=sunscanFiles(jj).bytes;
-    end
-
-    sunscanTimeDiff=etime(datevec(sunscanTimes(2:end)),datevec(sunscanTimes(1:end-1)));
-    sunscanDataRate=sunscanBytes(1:end-1)./sunscanTimeDiff;
-
-    if ~isempty(sunscanTimes)
-        goodTimes=zeros(size(sunscanTimeDiff));
-        goodTimes(sunscanTimeDiff<120)=1;
-        diffGoodTimes=diff(goodTimes);
-
-        startGoodInds=find(diffGoodTimes==1)+1;
-        endGoodInds=find(diffGoodTimes==-1)+1;
-
-        startGoodTimes=sunscanTimes(startGoodInds);
-        startGoodTimes=cat(1,sunscanTimes(1),startGoodTimes);
-        endGoodTimes=sunscanTimes(endGoodInds);
-        endGoodTimes=cat(1,endGoodTimes,sunscanTimes(end));
-
-        for kk=1:length(startGoodTimes)
-            noDataTimeNan(noDataTime>startGoodTimes(kk) & noDataTime<endGoodTimes(kk))=nan;
-        end
-
-    end
     % 'vert' directory
-    vertFiles=dir([baseDir,'vert/',dateString,'/*.iwrf_ts']);
-    vertTimes=[];
-    vertBytes=nan(length(vertFiles),1);
-
-    for jj=1:size(vertFiles,1)
-        inName=vertFiles(jj).name;
-        vertTimes=[vertTimes;datetime(str2num(inName(1:4)),str2num(inName(5:6)),str2num(inName(7:8)),...
-            str2num(inName(10:11)),str2num(inName(12:13)),str2num(inName(14:15)),str2num(inName(17:19)))];
-        vertBytes(jj)=vertFiles(jj).bytes;
-    end
-
-    vertTimeDiff=etime(datevec(vertTimes(2:end)),datevec(vertTimes(1:end-1)));
-    vertDataRate=vertBytes(1:end-1)./vertTimeDiff;
-
-    if ~isempty(vertTimes)
-        goodTimes=zeros(size(vertTimeDiff));
-        goodTimes(vertTimeDiff<120)=1;
-        diffGoodTimes=diff(goodTimes);
-
-        startGoodInds=find(diffGoodTimes==1)+1;
-        endGoodInds=find(diffGoodTimes==-1)+1;
-
-        startGoodTimes=vertTimes(startGoodInds);
-        startGoodTimes=cat(1,vertTimes(1),startGoodTimes);
-        endGoodTimes=vertTimes(endGoodInds);
-        endGoodTimes=cat(1,endGoodTimes,vertTimes(end));
-    end
-
-    for kk=1:length(startGoodTimes)
-        noDataTimeNan(noDataTime>startGoodTimes(kk) & noDataTime<endGoodTimes(kk))=nan;
-    end
+    [vertTimes,vertDataRate,noDataTimeNan]=findGaps_timeSeries('vert',baseDir,dateString,noDataTime,noDataTimeNan);
 
     %% Plot
 
@@ -140,8 +50,8 @@ for ii=1:length(dayList)
     hold on
     scatter(saveTimes(1:end-1),saveDataRate./1000000,'filled','blue');
     scatter(sunscanTimes(1:end-1),sunscanDataRate./1000000,'filled','green');
-    scatter(vertTimes(1:end-1),vertDataRate./1000000,'filled','cyan');
-    scatter(noDataTime,noDataTimeNan,'filled','red');
+    scatter(vertTimes(1:end-1),vertDataRate./1000000,'filled','magenta');
+    scatter(noDataTime,noDataTimeNan,'+','red');
     ylim(ylimits);
     xlim([thisDay,thisDay+hours(6)]);
     ylabel('MB s^{-1}');
@@ -153,8 +63,8 @@ for ii=1:length(dayList)
     hold on
     scatter(saveTimes(1:end-1),saveDataRate./1000000,'filled','blue');
     scatter(sunscanTimes(1:end-1),sunscanDataRate./1000000,'filled','green');
-    scatter(vertTimes(1:end-1),vertDataRate./1000000,'filled','cyan');
-    scatter(noDataTime,noDataTimeNan,'filled','red');
+    scatter(vertTimes(1:end-1),vertDataRate./1000000,'filled','magenta');
+    scatter(noDataTime,noDataTimeNan,'+','red');
     ylim(ylimits);
     xlim([thisDay+hours(6),thisDay+hours(12)]);
     ylabel('MB s^{-1}');
@@ -165,8 +75,8 @@ for ii=1:length(dayList)
     hold on
     scatter(saveTimes(1:end-1),saveDataRate./1000000,'filled','blue');
     scatter(sunscanTimes(1:end-1),sunscanDataRate./1000000,'filled','green');
-    scatter(vertTimes(1:end-1),vertDataRate./1000000,'filled','cyan');
-    scatter(noDataTime,noDataTimeNan,'filled','red');
+    scatter(vertTimes(1:end-1),vertDataRate./1000000,'filled','magenta');
+    scatter(noDataTime,noDataTimeNan,'+','red');
     ylim(ylimits);
     xlim([thisDay+hours(12),thisDay+hours(18)]);
     ylabel('MB s^{-1}');
@@ -177,8 +87,8 @@ for ii=1:length(dayList)
     hold on
     scatter(saveTimes(1:end-1),saveDataRate./1000000,'filled','blue');
     scatter(sunscanTimes(1:end-1),sunscanDataRate./1000000,'filled','green');
-    scatter(vertTimes(1:end-1),vertDataRate./1000000,'filled','cyan');
-    scatter(noDataTime,noDataTimeNan,'filled','red');
+    scatter(vertTimes(1:end-1),vertDataRate./1000000,'filled','magenta');
+    scatter(noDataTime,noDataTimeNan,'+','red');
     ylim(ylimits);
     xlim([thisDay+hours(18),thisDay+hours(24)]);
     ylabel('MB s^{-1}');
