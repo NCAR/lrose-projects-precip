d6c7726b1ef6dcc532c4130f1d987b5fd0743566
dixon <dixon@ucar.edu>
2023-02-27 08:48:14 -0700

    PlotFieldBias.py

--- projDir/qc/scripts/PlotFieldBias.py
+++ projDir/qc/scripts/PlotFieldBias.py
@@ -72,23 +72,7 @@ def main():
                       dest='endTime',
                       default='2022 08 11 00 00 00',
                       help='End time for XY plot')
-    parser.add_option('--zdrStatsStartTime',
-                      dest='zdrStatsStartTime',
-                      default='2022 05 25 03 00 00',
-                      help='Start time for computing ZDR stats')
-    parser.add_option('--zdrStatsEndTime',
-                      dest='zdrStatsEndTime',
-                      default='2022 07 09 00 00 00',
-                      help='End time for computing ZDR stats')
-    parser.add_option('--zdrMin',
-                      dest='zdrMin',
-                      default=-2.0,
-                      help='Min zdr in upper plot')
-    parser.add_option('--zdrMax',
-                      dest='zdrMax',
-                      default=2.0,
-                      help='Max zdr in upper plot')
-    
+
     (options, args) = parser.parse_args()
     
     if (options.verbose):
@@ -102,21 +86,11 @@ def main():
     endTime = datetime.datetime(int(year), int(month), int(day),
                                 int(hour), int(minute), int(sec))
 
-    year, month, day, hour, minute, sec = options.zdrStatsStartTime.split()
-    zdrStatsStartTime = datetime.datetime(int(year), int(month), int(day),
-                                          int(hour), int(minute), int(sec))
-
-    year, month, day, hour, minute, sec = options.zdrStatsEndTime.split()
-    zdrStatsEndTime = datetime.datetime(int(year), int(month), int(day),
-                                        int(hour), int(minute), int(sec))
-
     if (options.debug):
         print("Running %prog", file=sys.stderr)
         print("  biasFilePath: ", options.biasFilePath, file=sys.stderr)
         print("  startTime: ", startTime, file=sys.stderr)
         print("  endTime: ", endTime, file=sys.stderr)
-        print("  zdrStatsStartTime: ", zdrStatsStartTime, file=sys.stderr)
-        print("  zdrStatsEndTime: ", zdrStatsEndTime, file=sys.stderr)
 
     # read in column headers for bias results
 
@@ -264,57 +238,11 @@ def doPlot():
     dbzBiasAv = movingAverage(dbzBias, lenMeanFilter)
     dbzBiasAv[dbzBiasAv > 5] = math.nan;
     dbzBiasAv[dbzBiasAv < -0.5] = math.nan;
-    #dbzBiasAv[:int(lenMeanFilter/2)] = math.nan;
-    #dbzBiasAv[int(-lenMeanFilter/2):] = math.nan;
 
     zdrBias = np.array(biasData["zdrDiff"]).astype(np.double)
     zdrBiasAv = movingAverage(zdrBias, lenMeanFilter)
     #zdrBiasAv[zdrBiasAv > 5] = math.nan;
     #zdrBiasAv[zdrBiasAv < -5] = math.nan;
-    #zdrBiasAv[:int(lenMeanFilter/2)] = math.nan;
-    #zdrBiasAv[int(-lenMeanFilter/2):] = math.nan;
-
-    # measuredDbmVc = np.array(biasData["measuredDbmVc"]).astype(np.double)
-    # measuredDbmVcAv = movingAverage(measuredDbmVc, lenMeanFilter)
-    # measuredDbmVcAv[measuredDbmVcAv > -66] = math.nan;
-    # measuredDbmVcAv[measuredDbmVcAv < -70] = math.nan;
-    # measuredDbmVcAv[:int(lenMeanFilter/2)] = math.nan;
-    # measuredDbmVcAv[int(-lenMeanFilter/2):] = math.nan;
-
-    # validDbzBias = np.logical_and(np.isfinite(dbzBiasAv),
-    #                               np.isfinite(measuredDbmVcAv))
-
-    # validMeasuredDbmNtimes = ntimes[validMeasuredDbm]
-    # validMeasuredDbmHcVals = dbzBiasAv[validMeasuredDbm]
-    # validMeasuredDbmVcVals = measuredDbmVcAv[validMeasuredDbm]
-    
-    # biasZdrVals = validMeasuredDbmHcVals - validMeasuredDbmVcVals
-
-    # vertZdrm = np.array(vertData["meanZdrmVol"]).astype(np.double)
-    # vertZdrmAv = movingAverage(vertZdrm, lenMeanFilter)
-    # validVertZdrm = np.isfinite(vertZdrmAv)
-    # validVertZdrmNtimes = vtimes[validVertZdrm]
-    # validVertZdrmVals = vertZdrmAv[validVertZdrm]
-
-    # compute the mean bias zdr and vert zdr for the stats time period
-
-    # statsDbmHc = dbzBias[np.logical_and(ntimes >= zdrStatsStartTime,
-    #                                           ntimes <= zdrStatsEndTime)]
-    # statsDbmVc = measuredDbmVc[np.logical_and(ntimes >= zdrStatsStartTime,
-    #                                           ntimes <= zdrStatsEndTime)]
-    # statsBiasZdr = statsDbmHc - statsDbmVc
-
-    # statsVertZdrm = vertZdrm[np.logical_and(vtimes >= zdrStatsStartTime,
-    #                                         vtimes <= zdrStatsEndTime)]
-    # biasZdrStatsMean = np.mean(statsBiasZdr)
-    # vertZdrmStatsMean = np.mean(statsVertZdrm)
-    # biasToZdrCorr = vertZdrmStatsMean - biasZdrStatsMean
-    # biasZdrValsCorr = biasZdrVals + biasToZdrCorr
-    
-    # if (options.debug):
-    #     print("  ==>> biasZdrStatsMean: ", biasZdrStatsMean, file=sys.stderr)
-    #     print("  ==>> vertZdrmStatsMean: ", vertZdrmStatsMean, file=sys.stderr)
-    #     print("  ==>> biasToZdrCorr: ", biasToZdrCorr, file=sys.stderr)
 
     # set up plots
 
@@ -339,42 +267,11 @@ def doPlot():
     ax1b.plot(ntimes, zdrBiasAv, \
               label = 'ZDR Bias', linewidth=1, color='black')
 
-    # ax1a.plot(validMeasuredDbmNtimes, biasZdrValsCorr, \
-    #           label = 'Bias ZDRm + biasToZdrCorr', linewidth=1, color='brown')
-    # ax1a.plot(validVertZdrmNtimes, validVertZdrmVals, \
-    #           ".", label = 'Vert ZDRm', color='green')
-
-    # ax1b.plot(validMeasuredDbmNtimes, validMeasuredDbmHcVals, \
-    #           label = 'Mean Bias DbmHc', linewidth=1, color='red')
-    
-    # ax1b.plot(validMeasuredDbmNtimes, validMeasuredDbmVcVals, \
-    #           label = 'Mean Bias DbmVc', linewidth=1, color='blue')
-    
     configDateAxis(ax1a, -9999, -9999, "DBZ BIAS (dB)", 'upper right')
-    #configDateAxis(ax1a, float(options.zdrMin), float(options.zdrMax), "ZDRm (dB)", 'upper right')
     configDateAxis(ax1b, -9999, -9999, "ZDR BIAS (dB)", 'upper right')
-    #configDateAxis(ax1b, float(options.biasMin), float(options.biasMax), "Bias Power (dBm)", 'upper right')
-
-    # add text labels
-
-    # label1 = "Stats start: " + zdrStatsStartTime.strftime('%Y-%m-%d')    
-    # label2 = "Stats end: " + zdrStatsEndTime.strftime('%Y-%m-%d')    
-    # label3 = "Ntimes smooth: " + str(options.lenMean)
-
-    # label4 = "biasZdrMean: " + ("%.2f" % biasZdrStatsMean)
-    # label5 = "vertZdrMean: " + ("%.2f" % vertZdrmStatsMean)
-    # label6 = "biasToZdrCorr: " + ("%.2f" % biasToZdrCorr)
 
     ax1a.set_facecolor("lightgrey")
     ax1b.set_facecolor("lightgrey")
-
-    # plt.figtext(0.06, 0.95, label1)
-    # plt.figtext(0.06, 0.93, label2)
-    # plt.figtext(0.06, 0.91, label3)
-
-    # plt.figtext(0.2, 0.95, label4)
-    # plt.figtext(0.2, 0.93, label5)
-    # plt.figtext(0.2, 0.91, label6)
 
     fig1.autofmt_xdate()
     fig1.tight_layout()
@@ -402,91 +299,6 @@ def configDateAxis(ax, miny, maxy, ylabel, legendLoc):
     ax.xaxis.set_major_formatter(hfmt)
     for tick in ax.xaxis.get_major_ticks():
         tick.label.set_fontsize(8) 
-
-########################################################################
-# get temp closest in time to the search time
-
-def getClosestTemp(noiseTime, tempTimes, obsTemps):
-
-    twoHours = datetime.timedelta(0.0, 7200.0)
-
-    validTimes = ((tempTimes > (noiseTime - twoHours)) & \
-                  (tempTimes < (noiseTime + twoHours)))
-
-    if (len(validTimes) < 1):
-        return (noiseTime, float('NaN'))
-    
-    searchTimes = tempTimes[validTimes]
-    searchTemps = obsTemps[validTimes]
-
-    if (len(searchTimes) < 1 or len(searchTemps) < 1):
-        return (noiseTime, float('NaN'))
-
-    minDeltaTime = 1.0e99
-    ttime = searchTimes[0]
-    temp = searchTemps[0]
-    for ii, temptime in enumerate(searchTimes, start=0):
-        deltaTime = math.fabs((temptime - noiseTime).total_seconds())
-        if (deltaTime < minDeltaTime):
-            minDeltaTime = deltaTime
-            temp = searchTemps[ii]
-            ttime = temptime
-
-    return (ttime, temp)
-
-########################################################################
-# compute daily stats for a variable
-
-def computeDailyStats(times, vals):
-
-    dailyTimes = []
-    dailyMeans = []
-
-    nptimes = np.array(times).astype(datetime.datetime)
-    npvals = np.array(vals).astype(np.double)
-
-    validFlag = np.isfinite(npvals)
-    timesValid = nptimes[validFlag]
-    valsValid = npvals[validFlag]
-    
-    startTime = nptimes[0]
-    endTime = nptimes[-1]
-    
-    startDate = datetime.datetime(startTime.year, startTime.month, startTime.day, 0, 0, 0)
-    endDate = datetime.datetime(endTime.year, endTime.month, endTime.day, 0, 0, 0)
-
-    oneDay = datetime.timedelta(1)
-    halfDay = datetime.timedelta(0.5)
-    
-    thisDate = startDate
-    while (thisDate < endDate + oneDay):
-        
-        nextDate = thisDate + oneDay
-        result = []
-        
-        sum = 0.0
-        sumDeltaTime = datetime.timedelta(0)
-        count = 0.0
-        for ii, val in enumerate(valsValid, start=0):
-            thisTime = timesValid[ii]
-            if (thisTime >= thisDate and thisTime < nextDate):
-                sum = sum + val
-                deltaTime = thisTime - thisDate
-                sumDeltaTime = sumDeltaTime + deltaTime
-                count = count + 1
-                result.append(val)
-        if (count > 5):
-            mean = sum / count
-            meanDeltaTime = datetime.timedelta(0, sumDeltaTime.total_seconds() / count)
-            dailyMeans.append(mean)
-            dailyTimes.append(thisDate + meanDeltaTime)
-            # print >>sys.stderr, " daily time, meanStrong: ", dailyTimes[-1], meanStrong
-            result.sort()
-            
-        thisDate = thisDate + oneDay
-
-    return (dailyTimes, dailyMeans)
-
 
 ########################################################################
 # Run a command in a shell, wait for it to complete
